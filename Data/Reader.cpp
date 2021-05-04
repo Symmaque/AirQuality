f113d36fce611e1b42f1971fa03b5344a1bad554
Marth83 <martin.collard@protonmail.com>
2021-05-04 15:22:21 +0200

    upload mesaures

--- Data/Reader.cpp
+++ Data/Reader.cpp
@@ -13,7 +13,21 @@ int main()
 void readSensor()
 {
     //File pointer
-    ifstream file("./data/sensors.csv");
+    ifstream file("./dataset/sensors.csv");
+    string buff;
+    if (file.is_open())
+    {
+        while (getline(file, buff, ';'))
+        {
+            cout << buff << ";";
+        }
+    }
+}
+
+void readCleaner()
+{
+    //File pointer
+    ifstream file("./dataset/cleaner.csv");
     string buff;
     if (file.is_open())
     {
